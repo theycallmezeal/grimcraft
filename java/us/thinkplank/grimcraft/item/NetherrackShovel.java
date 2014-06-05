9675cc9d2aa457526e42cee19bb212ae22f2a8cc
fwilson <fwilson@fwilson.me>
2014-06-05 11:57:40 -0400

    yolo deal with it

--- java/us/thinkplank/grimcraft/item/NetherrackShovel.java
+++ java/us/thinkplank/grimcraft/item/NetherrackShovel.java
@@ -4,7 +4,7 @@ import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemShovel;
 
-public class ItemNetherrackShovel extends ItemShovel {
+public class ItemNetherrackShovel extends ItemSpade {
     public ItemNetherrackShovel(ToolMaterial material) {
         super(material);
         setMaxStackSize(64);
