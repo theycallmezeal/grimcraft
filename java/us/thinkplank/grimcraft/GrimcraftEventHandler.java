53402c52a7ee43ed8f550cbb7bb94590fe987dbc
Lapiman <lapiman@thinkplank.us>
2014-06-30 19:39:52 -0400

    Hoeing soulsand - unsuccessful

--- java/us/thinkplank/grimcraft/GrimcraftEventHandler.java
+++ java/us/thinkplank/grimcraft/GrimcraftEventHandler.java
@@ -1,11 +1,19 @@
 package us.thinkplank.grimcraft;
 
+import us.thinkplank.grimcraft.block.BlockPeat;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class GrimcraftEventHandler {
 	
 	@SubscribeEvent
-	public void dummyMethod() {
+	public void onUseHoe(UseHoeEvent event) {
+		Block targetBlock = event.world.getBlock(event.x, event.y, event.z);
 		
+		if (targetBlock.equals(Blocks.soul_sand)) {
+			event.world.setBlock(event.x, event.y, event.z, new BlockPeat());
+		}
 	}
 }
