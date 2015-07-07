2b7e8be1bd67a10f6ad731ea082f7102d35e070a
Lapiman <lapiman@thinkplank.us>
2015-07-07 18:28:34 -0400

    Stairs kinda work, logs don't crash

--- java/us/thinkplank/grimcraft/block/BlockGrimwoodLog.java
+++ java/us/thinkplank/grimcraft/block/BlockGrimwoodLog.java
@@ -2,11 +2,13 @@ package us.thinkplank.grimcraft.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLog;
+import net.minecraft.block.BlockNewLog;
+import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockGrimwoodLog extends BlockLog {
+public class BlockGrimwoodLog extends BlockNewLog {
 	@SideOnly(Side.CLIENT)
 	
 	public BlockGrimwoodLog () {
