19710e5fe0be57a270e53dd2fc370d829b415700
Adam <zealan@gmail.com>
2016-07-28 14:16:51 -0400

    for now, remove slabs

--- java/us/thinkplank/grimcraft/block/BlockGrimwoodSlab.java
+++ java/us/thinkplank/grimcraft/block/BlockGrimwoodSlab.java
@@ -23,13 +23,11 @@ public abstract class BlockGrimwoodSlab extends BlockSlab {
 	
 	@Override
 	public IProperty<?> getVariantProperty() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Comparable<?> getTypeForItem(ItemStack stack) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 }
