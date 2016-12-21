ba9c87a5a6884b8e740a6600694ee1095251b4f1
Adam <zealan@gmail.com>
2016-12-21 14:09:29 -0500

    fix log rendering

--- java/us/thinkplank/grimcraft/block/BlockGrimwoodLog.java
+++ java/us/thinkplank/grimcraft/block/BlockGrimwoodLog.java
@@ -1,16 +1,17 @@
 package us.thinkplank.grimcraft.block;
 
-import net.minecraft.block.BlockOldLog;
+import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockGrimwoodLog extends BlockOldLog {
+public class BlockGrimwoodLog extends BlockRotatedPillar {
 	@SideOnly(Side.CLIENT)
 	
 	public BlockGrimwoodLog () {
-        super();
+        super(Material.WOOD);
         setHardness(2F);
         setSoundType(SoundType.WOOD);
         setRegistryName("grimwood_log");
