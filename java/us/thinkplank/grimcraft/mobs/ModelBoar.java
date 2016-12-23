e7469c8186f40890cffee993290d209e17148e3d
Adam <zealan@gmail.com>
2016-12-23 14:32:34 -0500

    remove unneeded files

--- java/us/thinkplank/grimcraft/mobs/ModelBoar.java
+++
@@ -1,7 +0,0 @@
-package us.thinkplank.grimcraft.mobs;
-
-import net.minecraft.client.model.ModelPig;
-
-public class ModelBoar extends ModelPig {
-	
-}
--- java/us/thinkplank/grimcraft/mobs/ModelPhoenix.java
+++
@@ -1,7 +0,0 @@
-//package us.thinkplank.grimcraft.mobs;
-//
-//import net.minecraft.client.model.ModelChicken;
-//
-//public class ModelPhoenix extends ModelChicken {
-//
-//}
