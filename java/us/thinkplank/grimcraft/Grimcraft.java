bc2fd1ff6b20b694ab6b71e69f5add6cbce1c616
Adam <zealan@gmail.com>
2017-01-08 00:45:49 -0500

    make the nether lantern transparent

--- java/us/thinkplank/grimcraft/Grimcraft.java
+++ java/us/thinkplank/grimcraft/Grimcraft.java
@@ -4,9 +4,13 @@
  * Licensed under whatever we decide
  * 
  * TODO
+ * the nether lantern doesn't light up the world
  * give nether foods a growth rate boost
  * ensure the same seed gives you the same ore locations every time
  * update ja_JP.lang
+ * 
+ * before final build:
+ * restore @Override
  */
 
 package us.thinkplank.grimcraft;
--- java/us/thinkplank/grimcraft/block/BlockNetherLantern.java
+++ java/us/thinkplank/grimcraft/block/BlockNetherLantern.java
@@ -3,6 +3,10 @@ package us.thinkplank.grimcraft.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockNetherLantern extends Block {
     public BlockNetherLantern () {
@@ -12,5 +16,18 @@ public class BlockNetherLantern extends Block {
         setRegistryName("nether_lantern");
         setHarvestLevel("pickaxe", 1);
         setLightLevel(15F);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+    
+    public boolean isOpaqueCube(IBlockState state) {
+    	return false;
     }
 }
